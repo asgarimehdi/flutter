6d89002de91f15700ae67a8f366bfeb656ac93b9
unknown <mehdi.asgari@gmail.com>
2022-07-14 21:01:16 +0430

    lesson 24

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -23,6 +24,10 @@ class Categories extends StatefulWidget {
 
 class _CategoriesState extends State<Categories> {
   late Future<List<Category>> futureCategories;
+  final _formKey = GlobalKey<FormState>();
+  late Category selectedCategory;
+  final CategoryNameController = TextEditingController();
+
   Future<List<Category>> fetchCategories() async {
     http.Response response =
         await http.get(Uri.parse('http://localhost/api/categories'));
@@ -30,6 +35,24 @@ class _CategoriesState extends State<Categories> {
     List categories = json.decode(response.body);
 
     return categories.map((category) => Category.fromJson(category)).toList();
+  }
+
+  Future saveCategory() async {
+    final form = _formKey.currentState;
+    if (!form!.validate()) {
+      return;
+    }
+    String uri =
+        'http://localhost/api/categories/' + selectedCategory.id.toString();
+    await http.put(Uri.parse(uri),
+        headers: {
+          HttpHeaders.contentTypeHeader: 'application/json',
+          HttpHeaders.acceptHeader: 'application/json',
+        },
+        body: json.encode({
+          'name': CategoryNameController.text,
+        }));
+    Navigator.pop(context);
   }
 
   @override
@@ -44,24 +67,57 @@ class _CategoriesState extends State<Categories> {
         appBar: AppBar(
           title: Text('Categories'),
         ),
-        body:  FutureBuilder<List<Category>>(
-              future: futureCategories,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return ListView.builder(
-                    itemCount: snapshot.data!.length,
-                    itemBuilder: (context, index) {
-                      Category category = snapshot.data![index];
-                      return ListTile(
-                        title: Text(category.name),
-                      );
-                    },
-                  );
-                } else if (snapshot.hasError) {
-                  return Text("${snapshot.error}");
-                }
-                return CircularProgressIndicator();
-              },
+        body: FutureBuilder<List<Category>>(
+          future: futureCategories,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  Category category = snapshot.data![index];
+                  return ListTile(
+                      title: Text(category.name),
+                      trailing: IconButton(
+                        icon: Icon(Icons.edit),
+                        onPressed: () {
+                          selectedCategory = category;
+                          CategoryNameController.text = category.name;
+                          showModalBottomSheet(
+                              context: context,
+                              builder: (context) {
+                                return Padding(
+                                    padding: EdgeInsets.all(10),
+                                    child: Form(
+                                        key: _formKey,
+                                        child: Column(children: <Widget>[
+                                          TextFormField(
+                                            controller: CategoryNameController,
+                                            validator: (String? value) {
+                                              if (value!.isEmpty) {
+                                                return 'enter category name';
+                                              }
+                                              return null;
+                                            },
+                                            decoration: InputDecoration(
+                                              border: OutlineInputBorder(),
+                                              labelText: 'Category Name',
+                                            ),
+                                          ),
+                                          ElevatedButton(
+                                            child: Text('Save'),
+                                            onPressed: () => saveCategory(),
+                                          )
+                                        ])));
+                              });
+                        },
+                      ));
+                },
+              );
+            } else if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            }
+            return CircularProgressIndicator();
+          },
         ));
   }
 }
